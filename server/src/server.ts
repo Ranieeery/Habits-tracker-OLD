160c50f98227f43652273c4e558fc19fd9137a65
Ranieeery <raniery2003@hotmail.com>
2023-02-07 08:40:34 -0300

    [new] frontend side

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,19 +1,16 @@
 // API RESTful
 import Fastify from "fastify";
+import cors from "@fastify/cors";
 import { PrismaClient } from "@prisma/client";
 
 const app = Fastify();
 const prisma = new PrismaClient();
 
+app.register(cors);
+
 //HTTP: Get, Post, Put, Patch, Delete
 app.get("/", async () => {
-  const habits = await prisma.habit.findMany({
-    where: {
-      title: {
-        startsWith: "Beber",
-      },
-    },
-  });
+  const habits = await prisma.habit.findMany();
 
   return habits;
 });
